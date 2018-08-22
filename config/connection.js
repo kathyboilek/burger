ca8101cd4b921bcf85d59aff6888c52520b9a6b9
Kathleen Boilek <kaboilek@gmail.com>
2018-08-22 08:18:44 -0400

    updated connection for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ var mysql = require('mysql');
 // Create the MySQL connection object
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-// 	// DB is JawsDB on Heroku
-// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 	// DB is local on localhost
 	connection = mysql.createConnection({
 		port: 3307,
@@ -18,7 +18,7 @@ var connection;
 		password: 'root',
 		database: 'burgers_db'
 	})
-// };
+};
 
 // Make the connection to MySQL
 connection.connect(function(err) {
